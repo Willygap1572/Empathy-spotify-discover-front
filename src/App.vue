c3e43846d8d7eb17487b9d7760aee05e7456a698
Willygap1572 <guillermoda@empathy.co>
2023-07-06 11:20:53 +0200

    initializing the front project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>List of Elements</h1>
+    <ul>
+      <li v-for="element in elements" :key="element.id">
+        {{ element }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      elements: []
+    };
+  },
+  mounted() {
+    this.fetchElements();
+  },
+  methods: {
+    fetchElements() {
+      axios.get('http://localhost:8080/searchDocument')
+        .then(response => {
+          this.elements = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
